cca2b9510bb6d29ce53c051da1da23e53eb56546
Jack <devjack050700@gmail.com>
2024-12-02 10:33:52 +0900

    고양이 정보 커뮤니티

--- project/src/cats/cats.controller.ts
+++ project/src/cats/cats.controller.ts
@@ -1,18 +1,34 @@
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
+import { HttpExceptionFilter } from 'src/common/http-exception.filter';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   //cats
   @Get()
   getAllCat() {
-    return 'all cat';
+    // throw new HttpException('api is broken', 401);
+    return 'get all cat';
   }
   // cats/:id
   @Get('id')
-  getOneCat() {
+  getOneCat(@Param('id', ParseIntPipe) param: number) {
+    console.log(param);
+    console.log(typeof param);
     return 'one cat';
   }
 
